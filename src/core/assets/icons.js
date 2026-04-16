97a63e91e4e2557614a250934dc9525a87fb6c68
Darshvoid <darshvoid@gmail.com>
2026-04-15 21:36:55 -0600

    Add exports for core icons

---
+++ src/core/assets/icons.js
@@ -0,0 +1,41 @@
+import closeSrc from "./svgs/close.svg";
+import bookSrc from "./svgs/Book.svg";
+import allTasksSrc from "./svgs/all-tasks.svg";
+import calendarImportantSrc from "./svgs/calendar-important.svg";
+import calendarWeekSrc from "./svgs/calendar-week.svg";
+import deleteSrc from "./svgs/delete.svg";
+import editSrc from "./svgs/edit.svg";
+import filterSrc from "./svgs/filter.svg";
+import githubSrc from "./svgs/github.svg";
+import homeSrc from "./svgs/home.svg";
+import inputTextSrc from "./svgs/input-text.svg";
+import newProjectSrc from "./svgs/new-project.svg";
+import projectPointerSrc from "./svgs/project-pointer.svg";
+import projectsSrc from "./svgs/projects.svg";
+import searchSrc from "./svgs/search.svg";
+import sidebarMinimizeSrc from "./svgs/sidebar-minimize.svg";
+import taskAddSrc from "./svgs/task-add.svg";
+import viewSrc from "./svgs/view.svg";
+
+const icons = {
+    close: closeSrc,
+    book: bookSrc,
+    allTasks: allTasksSrc,
+    calendarImportant: calendarImportantSrc,
+    calendarWeek: calendarWeekSrc,
+    delete: deleteSrc,
+    edit: editSrc,
+    filter: filterSrc,
+    github: githubSrc,
+    home: homeSrc,
+    inputText: inputTextSrc,
+    newProject: newProjectSrc,
+    projectPointer: projectPointerSrc,
+    projects: projectsSrc,
+    search: searchSrc,
+    sidebarMinimize: sidebarMinimizeSrc,
+    taskAdd: taskAddSrc,
+    view: viewSrc
+};
+
+export default icons;
--- src/core/exports/index.js
+++ src/core/exports/index.js
@@ -2,12 +2,14 @@ import ui from "../ui/index.js";
 import utils from "../utils/index.js"
 import lib from "../lib/index.js"
 import components from "../components/index.js"
+import icons from "../assets/icons.js"
 
 let core = {
     ui,
     utils,
     lib,
-    components
+    components,
+    icons
 }
 
 export default core;
